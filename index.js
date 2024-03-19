ef853d4b21a06f0e918f865804bb90b7b717bebc
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-19 22:58:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import inqurier from "inquirer";
+// Asking Questions from users through Inqurier
+const answer = await inqurier.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Additional", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(answer);
+//coditional statements IF-ELSE
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
